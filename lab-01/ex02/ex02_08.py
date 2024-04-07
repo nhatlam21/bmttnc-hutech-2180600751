c63ca7a39bff7cb7b0fbd0f3316bf1eb9a38bc4c
nhatlam21 <lamnhatle357@gmail.com>
2024-04-07 11:44:38 +0700

    [add] lab-01 ex01 ex02

--- lab-01/ex02/ex02_08.py
+++ lab-01/ex02/ex02_08.py
@@ -3,8 +3,7 @@ def chia_het_cho_5(so_nhi_phan):
     if so_thap_phan %5 == 0:
         return True 
     else:
-        return False
-      
+        return False    
     chuoi_so_nhi_phan = input("Nhập chuỗi số nhị phân (phân tách bởi dấu phẩy):")
     so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
     so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
@@ -13,4 +12,3 @@ def chia_het_cho_5(so_nhi_phan):
         print("Các số nhị phân chia hết cho 5 là:", ket_qua)
     else:
         print("Không có số nhị phân nào chia hết cho 5 trong chuỗi đã nhập. ")
-    
